e4511b10bcee862f0c196fa45f486b40ead59e42
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-10-16 10:44:18 -0700

    sscanf is now guarded in THMXParser.cpp file.

--- src/thmxParser.cpp
+++ src/thmxParser.cpp
@@ -97,7 +97,11 @@ namespace thmxParser
         // int shadeID = std::stoi( str );
         str = getAttribute(materialNode, "RGBColor");
         unsigned int red, green, blue;
+#ifdef _MSC_VER
+        sscanf_s(str.c_str(), "0x%02x%02x%02x", &red, &green, &blue);
+#else
         std::sscanf(str.c_str(), "0x%02x%02x%02x", &red, &green, &blue);
+#endif
         ColorRGB color{red, green, blue};
         int cavityModel = 0;
         str = getAttribute(materialNode, "CavityModel");
